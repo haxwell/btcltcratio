4c80284c0d77ac04eb85f8b8afabfa3cf0d0a910
Johnathan <haxwell@gmail.com>
2015-10-02 11:12:00 -0600

    Added variables for the various cached time periods

--- app/controllers/btcltcratio_controller.rb
+++ app/controllers/btcltcratio_controller.rb
@@ -1,5 +1,8 @@
+require 'pubticker_cachers/time_period_constants'
+
 class BtcltcratioController < ApplicationController
     def index 
+        # Get the most recent pubticker value from the DB
         @btc = JSON.parse Pubticker.where('tickerSymbol' => 'btcusd').order('id desc').limit(1).to_json
         @ltc = JSON.parse Pubticker.where('tickerSymbol' => 'ltcusd').order('id desc').limit(1).to_json
 
@@ -8,6 +11,20 @@ class BtcltcratioController < ApplicationController
 
         # TODO read from cached database records instead.
         @ratio = @btc['last_price'] / @ltc['last_price']
+
+        @ratio_minute = getCachedPubticker(TimePeriodConstants::MINUTE)
+        @ratio_hour = getCachedPubticker(TimePeriodConstants::HOUR)
+        @ratio_half_hour = getCachedPubticker(TimePeriodConstants::HALF_HOUR)
+        @ratio_half_day = getCachedPubticker(TimePeriodConstants::HALF_DAY)
+        @ratio_day = getCachedPubticker(TimePeriodConstants::DAY)
+    end
+
+    def getCachedPubticker(timeperiod, tickerSymbolA = 'btcusd', tickerSymbolB = 'ltcusd')
+        v = JSON.parse CachedPubticker.where(:timeperiod => timeperiod, :ticker_symbol_a => tickerSymbolA, :ticker_symbol_b => tickerSymbolB).to_json
+
+        puts v[0]
+
+        v[0]
     end
 
 end
