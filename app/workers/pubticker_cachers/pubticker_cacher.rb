22efd29b0df21f70d0ea43cb76f3b44e0ef2ec7b
Johnathan <haxwell@gmail.com>
2015-10-02 19:45:51 -0600

    Fixed pubticker_cacher code.. it runs without error now.. next step, test the validity of its calculations

--- app/workers/pubticker_cachers/pubticker_cacher.rb
+++ app/workers/pubticker_cachers/pubticker_cacher.rb
@@ -20,25 +20,36 @@ class PubtickerCacher
 
     private
     def getAverageLowAndHighRatios(timePeriod, tickerSymbolA, tickerSymbolB)
-        # NOTE this all works because I assume that the timestamp (from bitfinex, not the Rails 'created at' stamp)
-        # of the newest row is the closest approximation I can get of when the database was last updated.
 
         #get the most recent row
-        a = Pubticker.order('timestamp desc').limit(1).to_json
+        a = Pubticker.order('created_at desc').limit(1).to_json
 
-        #using its created by timestamp
+        puts "--> " + a.to_s
+        
         aa = JSON.parse a
         aa = aa[0]
-        periodEndDate = Time.at(aa['timestamp']).to_datetime
+        periodEndTime = DateTime.parse(aa['created_at']).to_time.utc
+
+        puts "periodEndTime: " + periodEndTime.to_s
 
         #get the rows previous to it that cover the time period
-        periodBeginDate = TimePeriodConstants.new.getBeginDate(timePeriod, periodEndDate)
+        periodBeginTime = TimePeriodConstants.new.getBeginDate(timePeriod, periodEndTime)
 
-        b = Pubticker.where(timestamp: (periodBeginDate.to_f)..(periodEndDate.to_f)).order('timestamp desc')
+        puts "periodBeginTime: " + periodBeginTime.to_s
+        puts "periodEndTime: " + periodEndTime.to_s
+
+        periodEndTime = periodEndTime + 1;
+
+        b = Pubticker.where(created_at: (periodBeginTime.to_s.sub(" UTC", ""))..(periodEndTime.to_s.sub(" UTC", ""))).order('created_at desc')
         ba = b.to_a
 
-        ba1 = ba.select { |x| x.tickerSymbol == tickerSymbolA }        
-        ba2 = ba.select { |x| x.tickerSymbol == tickerSymbolB }        
+        puts "Query returned " + ba.size.to_s + " rows."
+
+        ba1 = ba.select { |x| x.tickerSymbol == tickerSymbolA }
+        ba2 = ba.select { |x| x.tickerSymbol == tickerSymbolB }
+
+        puts "tickerSymbolA size = " + ba1.size.to_s
+        puts "tickerSymbolB size = " + ba2.size.to_s
 
         # ensure arrays are the same size
         unless ba1.size == ba2.size 
@@ -48,8 +59,10 @@ class PubtickerCacher
         # send arrays of pubticker objects to calculator
         ratios = PeriodRatioCalculator.new.calculate(ba1, ba2)
 
+        puts "Calculated Ratios: " + ratios.to_s
+
         # add some extra columns
-        ratios = ratios.merge({:ticker_symbol_a => tickerSymbolA, :ticker_symbol_b => tickerSymbolB, :period_begin => periodBeginDate.to_f, :period_end => periodEndDate.to_f, :timeperiod => timePeriod})
+        ratios = ratios.merge({:ticker_symbol_a => tickerSymbolA, :ticker_symbol_b => tickerSymbolB, :period_begin => periodBeginTime.to_f, :period_end => periodEndTime.to_f, :timeperiod => timePeriod})
     end
 
 end
--- app/workers/pubticker_cachers/time_period_constants.rb
+++ app/workers/pubticker_cachers/time_period_constants.rb
@@ -9,21 +9,19 @@ class TimePeriodConstants
 #    THIRTY_DAYS = 8
 
     def initialize 
-    # I use DateTime.parse because it loses the milliseconds.. simplifies queries, I think.
-
-        @map = { MINUTE => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60).to_s) },
-                 HALF_HOUR => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 30).to_s) },
-                 HOUR => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 60).to_s) },
-                 HALF_DAY => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 60 * 12).to_s) },
-                 DAY => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 60 * 24).to_s) },
-                 THREE_DAYS => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 60 * 24 * 3).to_s) },
-                 WEEK => lambda{ |periodEndDateTime| DateTime.parse(periodEndDateTime.ago(60 * 60 * 24 * 7).to_s) }
+        @map = { MINUTE => lambda{ |periodEndTime| (periodEndTime - 60).to_s },
+                 HALF_HOUR => lambda{ |periodEndTime| (periodEndTime - (60 * 30)).to_s },
+                 HOUR => lambda{ |periodEndTime| (periodEndTime - (60 * 60)).to_s },
+                 HALF_DAY => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 12)).to_s },
+                 DAY => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24)).to_s },
+                 THREE_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 3)).to_s },
+                 WEEK => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 7)).to_s }
         }
     end
 
-    def getBeginDate(timePeriod, endDate)
-        unless endDate.is_a?(DateTime)
-            raise ArgumentError, 'endDate object should be instance of DateTime'
+    def getBeginDate(timePeriod, endTime)
+        unless endTime.is_a?(Time)
+            raise ArgumentError, 'endTime object should be instance of Time'
         end
 
         func = @map.fetch(timePeriod)
@@ -32,6 +30,6 @@ class TimePeriodConstants
             raise ArgumentError, 'invalid time period, dont know how to process it.. :('
         end
 
-        func.call(endDate.to_datetime)
+        func.call(endTime)
     end
 end
