7460eb8c74ae7afe548958aa32c6e0cb2184938a
Johnathan <haxwell@gmail.com>
2015-10-03 11:43:29 -0600

    Added PubtickerCachers to cover periods between a week and a year

--- app/models/pubticker_cacher_list.rb
+++ app/models/pubticker_cacher_list.rb
@@ -5,13 +5,21 @@ require 'pubticker_cachers/half_day_pubticker_cacher'
 require 'pubticker_cachers/day_pubticker_cacher'
 require 'pubticker_cachers/three_days_pubticker_cacher'
 require 'pubticker_cachers/week_pubticker_cacher'
+require 'pubticker_cachers/thirty_days_pubticker_cacher'
+require 'pubticker_cachers/sixty_days_pubticker_cacher'
+require 'pubticker_cachers/ninety_days_pubticker_cacher'
+require 'pubticker_cachers/one_hundred_eighty_days_pubticker_cacher'
+require 'pubticker_cachers/year_pubticker_cacher'
 
 class PubtickerCacherList
     def list
             a = [ MinutePubtickerCacher.new,
                 HalfHourPubtickerCacher.new, HourPubtickerCacher.new,
                 HalfDayPubtickerCacher.new, DayPubtickerCacher.new, ThreeDaysPubtickerCacher.new,
-                WeekPubtickerCacher.new]
+                WeekPubtickerCacher.new, ThirtyDaysPubtickerCacher.new, SixtyDaysPubtickerCacher.new, NinetyDaysPubtickerCacher.new,
+                OneHundredEightyDaysPubtickerCacher.new, YearPubtickerCacher.new
+            ]
+
             a
     end
 end
--- app/workers/pubticker_cachers/time_period_constants.rb
+++ app/workers/pubticker_cachers/time_period_constants.rb
@@ -6,7 +6,11 @@ class TimePeriodConstants
     DAY = 5
     THREE_DAYS = 6
     WEEK = 7
-#    THIRTY_DAYS = 8
+    THIRTY_DAYS = 8
+    SIXTY_DAYS = 9
+    NINETY_DAYS = 10
+    ONE_HUNDRED_EIGHTY_DAYS = 11
+    YEAR = 12
 
     def initialize 
         @map = { MINUTE => lambda{ |periodEndTime| (periodEndTime - 60).to_s },
@@ -15,7 +19,12 @@ class TimePeriodConstants
                  HALF_DAY => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 12)).to_s },
                  DAY => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24)).to_s },
                  THREE_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 3)).to_s },
-                 WEEK => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 7)).to_s }
+                 WEEK => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 7)).to_s },
+                 THIRTY_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 30)).to_s },
+                 SIXTY_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 60)).to_s },
+                 NINETY_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 90)).to_s },
+                 ONE_HUNDRED_EIGHTY_DAYS => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 180)).to_s },
+                 YEAR => lambda{ |periodEndTime| (periodEndTime - (60 * 60 * 24 * 365)).to_s }
         }
 
         @idToNameMap = { MINUTE => "minute",
@@ -24,7 +33,12 @@ class TimePeriodConstants
                          HALF_DAY => "half day",
                          DAY => "day",
                          THREE_DAYS => "3 days",
-                         WEEK => "week"
+                         WEEK => "week",
+                         THIRTY_DAYS => "30 days",
+                         SIXTY_DAYS => "60 days",
+                         NINETY_DAYS => "90 days",
+                         ONE_HUNDRED_EIGHTY_DAYS => "180 days",
+                         YEAR => "year"
         }
                     
     end
