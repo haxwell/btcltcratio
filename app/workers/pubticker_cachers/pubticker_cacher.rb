fa24ebd1b962194c929ccf800c511906708ee693
Johnathan <haxwell@gmail.com>
2015-10-04 14:01:17 -0600

    Removed duplicate code, now using getPeriodBeginAndEndTime() method

--- app/workers/pubticker_cachers/pubticker_cacher.rb
+++ app/workers/pubticker_cachers/pubticker_cacher.rb
@@ -58,24 +58,10 @@ class PubtickerCacher
     private
     def getAverageLowAndHighRatios(timePeriod, tickerSymbolA, tickerSymbolB)
 
-        #get the most recent row
-        a = Pubticker.order('created_at desc').limit(1).to_json
+        times = getPeriodBeginAndEndTime(timePeriod)
 
-        #puts "--> " + a.to_s
-        
-        aa = JSON.parse a
-        aa = aa[0]
-        periodEndTime = DateTime.parse(aa['created_at']).to_time.utc
-
-        #puts "periodEndTime: " + periodEndTime.to_s
-
-        #get the rows previous to it that cover the time period
-        periodBeginTime = TimePeriodConstants.new.getBeginDate(timePeriod, periodEndTime)
-
-        #puts "periodBeginTime: " + periodBeginTime.to_s
-        #puts "periodEndTime: " + periodEndTime.to_s
-
-        periodEndTime = periodEndTime + 1;
+        periodBeginTime = times[0] + 0;
+        periodEndTime = times[1] + 1;
 
         b = Pubticker.where(created_at: (periodBeginTime.to_s.sub(" UTC", ""))..(periodEndTime.to_s.sub(" UTC", ""))).order('created_at desc')
         ba = b.to_a
@@ -85,9 +71,6 @@ class PubtickerCacher
         ba1 = ba.select { |x| x.tickerSymbol == tickerSymbolA }
         ba2 = ba.select { |x| x.tickerSymbol == tickerSymbolB }
 
-        #puts "tickerSymbolA size = " + ba1.size.to_s
-        #puts "tickerSymbolB size = " + ba2.size.to_s
-
         # ensure arrays are the same size
         unless ba1.size == ba2.size 
             raise ArgumentError, "hmmm. something abnormal happened; the tickersymbol lists should be the same size"
@@ -96,10 +79,12 @@ class PubtickerCacher
         # send arrays of pubticker objects to calculator
         ratios = PeriodRatioCalculator.new.calculate(ba1, ba2)
 
-        puts "Calculated Ratios: " + ratios.to_s
-
         # add some extra columns
         ratios = ratios.merge({:ticker_symbol_a => tickerSymbolA, :ticker_symbol_b => tickerSymbolB, :period_begin => periodBeginTime.to_f, :period_end => periodEndTime.to_f, :timeperiod => timePeriod})
+
+        puts "Calculated Ratios: " + ratios.to_s
+
+        ratios
     end
 
     def getTimePeriod
