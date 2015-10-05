af6eedfeedee14fbac3305e8d0000f7e86646c64
Johnathan <haxwell@gmail.com>
2015-10-04 20:48:43 -0600

    Removed the front end of the application. Now, only the DB updating and BFX polling threads run. The UI is in another project. So this can run and run, while changes are made to the UI. Ideally, this back end should never stop. Ideally.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :btcltcratio
+#  resources :btcltcratio
 
   # You can have the root of your site routed with "root"
   root 'home#show'
